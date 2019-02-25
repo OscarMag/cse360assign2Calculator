71184ee25f7e5e3c08b5771af5369f89242f359b
oscma <oscma@OscarMPC.domain>
2019-02-24 19:17:55 -0700

    Completed getHistory method and updated documentation

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,7 +1,7 @@
 package cse360assign2;
 
-/* 
- * Author: Oscar Magaña
+/** 
+ * @author Oscar Magaña
  * Class ID: 465
  * Assignment #: 2
  * Description: The calculator class contains several methods that perform
@@ -12,39 +12,69 @@ public class Calculator
 {
 
 	private int total;
+	private String history;
 	
-	//Default constructor
+	/**
+	 * Default class constructor
+	 */
 	public Calculator () 
 	{
-		total = 0;  // not needed - included for clarity
+		total = 0;
+		history = "0 ";
 	}
 	
-	//The getTotal method returns the current total.
+	/**
+	 * Returns the current, running total
+	 * 
+	 * @return the value of total
+	 */
 	public int getTotal () 
 	{
 		return total;
 	}
 	
-	//The add method adds 'value' to 'total'.
+	/**
+	 *  Adds 'value' to 'total' and updates the history string
+	 *  with an addition sign and 'value'
+	 *  
+	 *  @param value to be added to total
+	 */
 	public void add (int value) 
 	{
 		total += value;
+		history += "+ " + value + " ";
 	}
 	
-	//The subtract method subtracts 'value' from 'total'.
+	/**
+	 * Subtracts 'value' from 'total' and updates the 
+	 * history string with a subtraction sign and 'value'
+	 * 
+	 * @param value to be subtracted from total
+	 */
 	public void subtract (int value) 
 	{
 		total -= value;
+		history += "- " + value + " ";
 	}
 	
-	//The multiply method multiplies 'total' by 'value'.
+	/**
+	 * Multiplies 'total' by 'value' and updates the
+	 * history string with a multiplication sign and 'value'
+	 * 
+	 * @param value which total is to be multiplied by
+	 */
 	public void multiply (int value) 
 	{
 		total *= value;
+		history += "* " + value + " ";
 	}
 	
-	/*The divide method divides 'total' by 'value' using integer division.
-	 *If 'value' is 0, 'total' becomes 0.
+	/**
+	 * Divides 'total' by 'value' using integer division, setting the total
+	 * to 0 if 'value' is 0, and updates the history string with a division
+	 * sign and 'value'
+	 * 
+	 * @param value which total is to be divided by
 	*/
 	public void divide (int value) 
 	{
@@ -52,11 +82,16 @@ public class Calculator
 			total = 0;
 		else 
 			total /= value;
+		
+		history += "/ " + value + " ";
 	}
 	
-	//The getHistory method returns a history of all actions as a string.
+	/**
+	 * The getHistory method returns a history of all actions as a string.
+	 * @return the history string
+	 */
 	public String getHistory () 
 	{
-		return "";
+		return history;
 	}
 }
