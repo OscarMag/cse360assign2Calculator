069ace71ebf7dcff49c0c7b01fa0eb9b2188d856
oscma <oscma@OscarMPC.domain>
2019-02-24 17:03:13 -0700

    Initial checkin

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,59 @@
+package cse360assign2;
+
+/* 
+ * Author: Oscar Magaña
+ * Class ID: 465
+ * Assignment #: 2
+ * Description: The calculator class contains several methods that perform
+ * the functions of a basic calculator.
+ */
+
+public class Calculator 
+{
+
+	private int total;
+	
+	//Default constructor
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//The getTotal method returns the current total.
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	//The add method adds 'value' to 'total'.
+	public void add (int value) 
+	{
+		
+	}
+	
+	//The subtract method subtracts 'value' from 'total'.
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	//The multiply method multiplies 'total' by 'value'.
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	/*The divide method divides 'total' by 'value' using integer division.
+	 *If 'value' is 0, 'total' becomes 0.
+	*/
+	public void divide (int value) 
+	{
+		
+	}
+	
+	//The getHistory method returns a history of all actions as a string.
+	public String getHistory () 
+	{
+		return "";
+	}
+}
